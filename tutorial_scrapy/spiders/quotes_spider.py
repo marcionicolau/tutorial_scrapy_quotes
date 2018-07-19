6d58abf9c74ed68b7fe1ec8586266e575fab9d88
Marcio Nicolau <marcionicolau@me.com>
2018-07-19 16:58:35 -0300

    Initial commit

---
+++ tutorial_scrapy/spiders/quotes_spider.py
@@ -0,0 +1,17 @@
+import scrapy
+
+
+class QuoteSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('small.author::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract(),
+            }
+
+        for href in response.css('li.next a::attr(href)'):
+            yield response.follow(href, callback=self.parse)
